a4ac33f624668c7138a9f558de9d7ff2d549ad95
IAMAcosta <agusstimakosta@gmail.com>
2025-12-17 18:49:01 -0300

    Fix: Arreglo conexion responsable de pago y metodo de busqueda
    corregido

--- src/api/responsable.api.ts
+++ src/api/responsable.api.ts
@@ -1,22 +1,41 @@
 import { PayerDTO, PersonaFisicaDTO, PersonaJuridicaDTO } from "@/src/dto/Facturacion.dto";
+import { Resultado } from "../dto/Resultado";
+import { ResponsableDePago } from "../dto/ResponsableDePago/ResponsableDePago.dto";
 
-const BASE_URL = "http://localhost:8080/ResponsableDePago";
+const BASE_URL = "http://localhost:8080/ResponsablePago";
+
+export interface BuscarResponsableDePagoRes {
+    resultado: Resultado;
+    responsableDePagos: ResponsableDePago[];
+}
 
 export const responsableApi = {
     // GET /ResponsableDePago/Buscar?cuit=20333...
-    buscarPorCuit: async (cuit: string): Promise<PayerDTO | null> => {
-        const response = await fetch(`${BASE_URL}/Buscar?cuit=${cuit}`);
-        if (response.status === 404) return null;
+    buscarPorCuit: async (cuit: string): Promise<ResponsableDePago[] | null> => {
+        const response = await fetch(`${BASE_URL}/ObtenerResponsablesDePago`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                cuit
+            }),
+        });
+
         if (!response.ok) throw new Error("Error al buscar responsable");
-        return await response.json();
+        const res: BuscarResponsableDePagoRes = await response.json();
+        if (res.resultado?.id !== 0) {
+            throw new Error(res.resultado?.mensaje || "Error desconocido en el servidor");
+        }
+        return res.responsableDePagos || null;
     },
 
     // POST /ResponsableDePago/Crear
-    crear: async (payer: Partial<PayerDTO>): Promise<PayerDTO> => {
-        const response = await fetch(`${BASE_URL}/Crear`, {
+    crear: async (payer: Partial<ResponsableDePago>): Promise<ResponsableDePago> => {
+        const response = await fetch(`${BASE_URL}/AltaResponsableDePago`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(payer),
+            body: JSON.stringify({
+                responsableDePagoDTO: payer
+            }),
         });
 
         if (!response.ok) {
